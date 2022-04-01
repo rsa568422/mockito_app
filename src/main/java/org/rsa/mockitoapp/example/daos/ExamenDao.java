5b37ea0a096280cb3f24657ea9137ec4732d0b16
Roberto Sánchez Arévalo <rsa568422@gmail.com>
2022-04-01 17:18:31 +0200

    Clase 33
    Se crea el nuevo proyecto para la sección 3. Se agregan las dependencias necesarias para esta sección en el pom.xml y se generan la clase Examen como POJO de la entidad examen y las interfaces ExamenDao y ExamenService con la declaración de los métodos a implementar.

---
+++ src/main/java/org/rsa/mockitoapp/example/daos/ExamenDao.java
@@ -0,0 +1,11 @@
+package org.rsa.mockitoapp.example.daos;
+
+import org.rsa.mockitoapp.example.models.Examen;
+
+import java.util.List;
+
+public interface ExamenDao {
+
+    List<Examen> finAll();
+
+}
---
+++ src/main/java/org/rsa/mockitoapp/example/services/ExamenService.java
@@ -0,0 +1,9 @@
+package org.rsa.mockitoapp.example.services;
+
+import org.rsa.mockitoapp.example.models.Examen;
+
+public interface ExamenService {
+
+    Examen finExamenByNombre(String nombre);
+
+}
